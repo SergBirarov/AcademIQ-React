43149dc711e2594857acbb68a0010b287e245d5c
barkan33 <mishakolomyza@gmail.com>
2024-10-06 10:28:43 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,43 +6,43 @@ import AppRouter from '../router/AppRouter';
 import Footer from './components/common/Footer';
 import { useAuth } from '../context/AuthContext';
 
-function App() { //  Добавьте  async 
+function App() {
   const { logout } = useAuth();
 
-  //TODO
-  // useEffect(async() => {
-  //   const token = localStorage.getItem('token');
-  //   const tokenCreated = localStorage.getItem('token_created');
+  //Get new token if expired
+  useEffect(async () => {
+    const token = localStorage.getItem('token');
+    const tokenCreated = localStorage.getItem('token_created');
 
-  //   if (token && tokenCreated) {
-  //     const timeRemaining = tokenCreated + 30 * 60 - Date.now();
-  //     if (timeRemaining < 0) {
-  //       logout();
-  //     } else {
-  //       try {
-  //         const response = await fetch("/api/User/newToken", {
-  //           method: "GET",
-  //           headers: {
-  //             "Authorization": `Bearer ${token}`,
-  //           },
-  //         });
+    if (token && tokenCreated) {
+      const timeRemaining = tokenCreated + 30 * 60 - Date.now();
+      if (timeRemaining <= 0) {
+        logout();
+      } else if (timeRemaining < 7) {
+        try {
+          const response = await fetch("/api/User/newToken", {
+            method: "GET",
+            headers: {
+              "Authorization": `Bearer ${token}`,
+            },
+          });
 
-  //         if (response.ok) {
-  //           const data = await response.json();
-  //           localStorage.setItem('token', data.token);
-  //           localStorage.setItem('token_created', Date.now());
-  //         } else {
-  //           localStorage.removeItem('token');
-  //           localStorage.removeItem('token_created');
-  //           logout();
-  //         }
-  //       } catch (error) {
-  //         console.error('Error refreshing token:', error);
-  //         logout();
-  //       }
-  //     }
-  //   }
-  // }, []);
+          if (response.ok) {
+            const data = await response.json();
+            localStorage.setItem('token', data.token);
+            localStorage.setItem('token_created', Date.now());
+          } else {
+            localStorage.removeItem('token');
+            localStorage.removeItem('token_created');
+            logout();
+          }
+        } catch (error) {
+          console.error('Error refreshing token:', error);
+          logout();
+        }
+      }
+    }
+  }, []);
 
   return (
     <ThemeProviderWrapper theme={theme}>
